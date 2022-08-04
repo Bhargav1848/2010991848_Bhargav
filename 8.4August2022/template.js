ac27ec5116c8416540918ba5a947786de124f090
Bhargav644 <bhargav1848.be20@chitkara.edu.in>
2022-08-04 13:32:56 +0530

    4 August 2022 added

---
+++ 8.4August2022/template.js
@@ -0,0 +1,27 @@
+function evenSum(n) {
+    var count = 0;
+    for (var i = 1; i < n; i++) {
+        if (i % 2 == 0) {
+            count++;
+        }
+    }
+
+    document.getElementById("text").innerHTML =
+        "Count of even numbers is: " + count;
+}
+
+function sumBetween(l, r, k) {
+    if (r < l) {
+        alert("R should be greater than L");
+        return;
+    }
+    var sum = 0;
+    for (var a = l; a < r; a++) {
+        if (a % k == 0) {
+            sum++;
+        }
+    }
+
+    document.getElementById("text").innerHTML =
+        "Count of numbers divisible by " + k + " : " + sum;
+}
