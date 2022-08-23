53b02bdd8fd0f2774bf54e93b6520e26d5c9d240
Bhargav644 <gautambhargav644@gmail.com>
2022-08-23 11:11:06 +0530

    23 August 2022 Error handling

---
+++ 11.23August2022/check.js
@@ -0,0 +1,36 @@
+var data = [{
+        id: 1,
+        name: "Bhargav",
+        age: 19,
+    },
+    {
+        id: 2,
+        name: "Ayerish",
+        age: 21,
+    },
+    {
+        id: 3,
+        name: "Amritansh",
+        age: 24,
+    },
+    {
+        id: 4,
+        name: "Chetan",
+        age: 20,
+    },
+];
+
+function check() {
+    var text = document.getElementById("text").value;
+    var name, id, age;
+    data.map((key, index) => {
+        if (key["id"] == text) {
+            name = key["name"];
+            age = key["age"];
+            id = key["id"];
+        }
+    });
+
+    document.getElementById("object").innerHTML =
+        " Id: " + id + "\n" + "Age: " + age + "\n" + " Name: " + name;
+}
---
+++ 11.23August2022/data.js
@@ -0,0 +1,22 @@
+const data = [{
+        id: 1,
+        name: "Bhargav",
+        age: 19,
+    },
+    {
+        id: 2,
+        name: "Ayerish",
+        age: 21,
+    },
+    {
+        id: 3,
+        name: "Amritansh",
+        age: 24,
+    },
+    {
+        id: 4,
+        name: "Chetan",
+        age: 20,
+    },
+];
+export default data;
