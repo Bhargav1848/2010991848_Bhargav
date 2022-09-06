e0419c0fb9d3ba01c2ec226c7dbe9f336a2e4047
Bhargav644 <gautambhargav644@gmail.com>
2022-09-06 16:03:09 +0530

    06 sept 2022

--- REACT/introtoreact/src/App.js
+++ REACT/introtoreact/src/App.js
@@ -4,17 +4,19 @@ import First from "./Components/First";
 import Login from "./Components/Login";
 import Registration from "./Components/Registration";
 import Welcome from "./Components/Welcome";
+import Style from "./Components/Style";
 function App() {
   return (
     <div className="App">
       {/* <First/> */}
       <div style={{ width: "50%" }}>
-        <h3>Task 1</h3>
+        {/* <h3>Task 1</h3>
         <div style={{ gap: "10%", display: "flex" }} id="task1">
           <Login />
           <Registration />
           <Welcome user="Bhargav" />
-        </div>
+        </div> */}
+        <Style />
       </div>
     </div>
   );
